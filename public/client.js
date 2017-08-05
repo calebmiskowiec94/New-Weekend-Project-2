0858ac2e5397ff0e3ac7e63c158b51f650c03789
Caleb <calebmiskowiec94@gmail.com>
2017-08-05 15:58:16 -0500

    put values into ajax object...i hope

--- public/client.js
+++ public/client.js
@@ -7,11 +7,31 @@ console.log('jquery works');
         var a= $('#valueOne').val()
         var b= $('#valueTwo').val()
         //console.log()
-        var sum = a-b;
+        //var sum = a-b;
        
         console.log(sum)
 
+$.ajax({
+           method:'POST',
+           //the '/' url has to match the app.post on app.js file
+           url:'/',
+           //data has to be an object
+           data:{
+               valueA: a,
+               valueB: b,
+               type: 'ADD'
+           },
+            //v v v response matches to the req, res 
+            //paramenters of the ajax function
+            success: function(response){
+                console.log(response)
+           
+            }
+       })
     });
+
+
+
 
 
 });//end of doc.ready
