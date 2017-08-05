7471c1816a23e0435d82fe1001892765618e859e
Caleb <calebmiskowiec94@gmail.com>
2017-08-05 15:14:47 -0500

    added first function for button

--- public/client.js
+++ public/client.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-console.log('jquery works')
+console.log('jquery works');
+
+//var a= $('#valueOne').val()
+//var b= $('#valueTwo').val()
+    $('#subtraction').on('click',function(){
+        var a= $('#valueOne').val()
+        var b= $('#valueTwo').val()
+        //console.log()
+        var sum = a-b;
+       
+        console.log(sum)
+
+    });
 
 
 });//end of doc.ready
+
+
+
+
+
