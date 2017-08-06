f554493a8371551ec8cd27d3d83f9d753c8a0be8
Caleb <calebmiskowiec94@gmail.com>
2017-08-06 16:01:02 -0500

    built out if else statements, doesnt work yet

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var port = 5000;
-var dom = []
+var dom = [];
+function calculate(typeOne,typeTwo,opp){
+    if (opp==="addition"){
+    dom = [+typeOne + +typeTwo];
+        
+    }
+    else if(opp==="subtraction"){
+        dom = [+typeOne - +typeTwo]
+        //do this
+    }
+    else if(opp==="multiplication"){
+        //do this
+        dom = [+typeOne * +typeTwo]
+
+    }
+    else{
+        //do this
+        dom = [+typeOne / +typeTwo]
+    }
+    
+}
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.post('/codeChallenge', function(req,res){
-//console.log(dom);
+console.log(dom);
 console.log(req.body)
+calculate(req.body.typeOne,req.body.typeTwo,req.body.opp);
 //v v v v  .send is only for (200)
 //dom.push(req.body) //if you dont add body parser to 
 //project req.body will be undefinda
